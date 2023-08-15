cdd020fef0d5e9f24de4a7bc0f34cf1637e782c5
30526939 <e.matos@pm.ba.gov.br>
2023-08-15 08:58:52 -0300

    curso php dio

---
+++ cursophp.php
@@ -0,0 +1,11 @@
+<?php
+
+//um entregador de delivery recebe 80 reais de pagamento por dia e tambem tem as gorgetas faça o código.
+
+$ValorDia = 80;
+$gorgetas = 40;
+
+$valorDiaria = $ValorDia + $gorgetas;
+echo "Segunda-Feira : $valorDiaria";
+
+?>
