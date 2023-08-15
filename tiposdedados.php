880a91bd65a91b6f58d8bdcbf856b7fa756ba136
30526939 <e.matos@pm.ba.gov.br>
2023-08-15 14:40:10 -0300

    alteração nova aula

--- tiposdedados.php
+++ tiposdedados.php
@@ -4,10 +4,14 @@
 //echo date("d"); //dia do mês.
 //echo date("m"); //mes do ano.
 //echo date("M"); //neste caso o mês aparece por extenso.
+//echo date("Y"); y minusculo só os ultimos dois digitos e Y maiusculo ele apresenta os 4 digitos.
+//echo date("d/m/Y"); dia mes ano;
 
+//salvando numa variavel 
+//date_default_timezone_set('America/Sao_Paulo');
 
-
-
+//$data = date("d/m/Y"); dia mes e ano
+//echo date("H:i:s");  hora minuto segundo
 
 
 
